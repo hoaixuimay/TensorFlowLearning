8193f135328e812c0c426f660ad916a437ff9db0
Hoai Pham <hoaixuimay@gmail.com>
2017-06-19 11:51:22 +0700

    init

---
+++ getting_start/tf.contrib.learn.custom.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import numpy as np
+import tensorflow as tf
+
+def model(features, labels, mode):
+	# Build a linear model and predict values
+	W = tf.get_variable("W", [1], dtype=tf.float64)
+	b = tf.get_variable("b", [1], dtype=tf.float64)
+	y = W * features['x'] + b
+	
+	# Lost sub-graph
+	loss = tf.reduce_sum(tf.square(y-labels))
+
+	# Training sub-graph
+	global_step = tf.train.get_global_step()
+	optimizer = tf.train.GradientDescentOptimizer(0.01)
+	train = tf.group(optimizer.minimize(loss), tf.assign_add(global_step, 1))
+	# ModelFnOpts
+	return tf.contrib.learn.ModelFnOps(
+		mode=mode,
+		predictions=y,
+		loss=loss,
+		train_op=train)
+
+estimator = tf.contrib.learn.Estimator(model_fn=model)
+# define our data set
+x = np.array([1.,2.,3.,4.])
+y = np.array([0.,-1.,-2.,-3.])
+input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x}, y, 4, num_epochs=1000)
+
+# train
+estimator.fit(input_fn=input_fn, steps=1000)
+# evaluate
+print(estimator.evaluate(input_fn=input_fn, steps = 10))
+
